660e2131a15aa41667405aec7502556d8a5029b1
David Husa <davidhusa@gmail.com>
2013-02-27 15:00:03 -0800

    Add ability to attach file to ticket

--- spec/integration/viewing_tickets_spec.rb
+++ spec/integration/viewing_tickets_spec.rb
@@ -9,6 +9,7 @@ feature "Viewing tickets" do
             :project => textmate_2,
             :title => "Make it shiny!",
             :description => "Gradients! Starbursts! Oh my!")
+    
     ticket.update_attribute(:user, user)
     internet_explorer = Factory(:project, :name => "Internet Explorer")
     define_permission!(user, "view", textmate_2)
@@ -31,5 +32,15 @@ feature "Viewing tickets" do
       page.should have_content("Make it shiny!")
     end
     page.should have_content("Gradients! Starbursts! Oh my!")
+  end
+
+  scenario "Ticket show page should only draw once" do
+    click_link "Sign out"
+    admin = Factory(:admin_user)
+    sign_in_as!(admin)
+    click_link "TextMate 2"
+    click_link "Make it shiny!"
+    puts page.body
+    page.should match_exactly(1, 'div#ticket')
   end
 end
